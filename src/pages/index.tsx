f352f0354c3085bc97514b7897e38eda112a099d
mantagen <contact.mantagen@gmail.com>
2020-11-23 11:59:53 +0000

    Remove 'in development' page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,13 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { PageProps } from "gatsby";
 import Layout from "../components/layout";
 import Carousel from "../components/carousel";
-import styled from "styled-components";
-
-import GlobalStyle from "../components/global-style";
-
-const Title = styled.h1`
-  padding-top: 32px;
-  padding-left: 38px;
-  padding-right: 38px;
-  padding-bottom: 34px;
-`;
-
-const DevelopmentSpan = styled.span`
-  color: #cecece;
-`;
 
 const Home: React.FC<PageProps> = (props) => {
   return (
-    <Fragment>
-      <GlobalStyle />
-      <Title>
-        Design Research Office
-        <br />
-        <DevelopmentSpan>Website in development</DevelopmentSpan>
-      </Title>
-    </Fragment>
+    <Layout>
+      <Carousel />
+    </Layout>
   );
 };
 
